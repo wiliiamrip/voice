4d29fdd8dc917b867054e83d3031f97e38694e0d
wiliiamrip <barojolayoussef@gmail.com>
2025-03-07 13:25:35 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "discord-bot",
+  "version": "1.0.0",
+  "main": "voice.js",
+  "scripts": {
+    "start": "node voice.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2",
+    "discord.js-selfbot-v13": "^2.15.1",
+    "request": "^2.88.2",
+    "concurrently": "^8.2.2",
+    "@discordjs/voice": "^0.16.1"
+  },
+  "engines": {
+    "node": "16.x"
+  }
+}
