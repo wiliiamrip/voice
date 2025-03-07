98374e7df78cbcd37fc1875380a996f7274a4637
wiliiamrip <barojolayoussef@gmail.com>
2025-03-07 13:45:44 +0000

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,31 +1,59 @@
+require('dotenv').config();
 const { Client } = require('discord.js-selfbot-v13');
-const client = new Client(); // All partials are loaded automatically
-
-client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!`);
-})
 const { joinVoiceChannel } = require('@discordjs/voice');
-client.on('ready', () => {
-    
-    setInterval(async () => {
-        client.channels.fetch('1344784289920979034') 
-            .then((channel) => { 
-                const VoiceConnection = joinVoiceChannel({
-                    channelId: channel.id, 
-                    guildId: channel.guild.id, 
-                    adapterCreator: channel.guild.voiceAdapterCreator,
-                    selfDeaf: false
-                });
-            }).catch((error) => { 
-                return; 
-            });
-    }, 1000)
-}); 
-const express = require("express");
-const app = express();
-app.listen(() => console.log("Server started"));
 
-app.use('/ping', (req, res) => {
-  res.send(new Date());
+const channels = ['1344784289920979034'];
+
+const checkAndJoin = async (client, channelId) => {
+  try {
+    const channel = client.channels.cache.get(channelId);
+    if (!channel) {
+      console.error('Channel not found with ID: ${channelId}');
+      return;
+    }
+
+    const guild = channel.guild;
+    const member = guild.members.cache.get(client.user.id);
+
+    if (!member) {
+      console.error('Bot is not a member of the guild (${guild.name}) associated with the channel ID: ${channelId}');
+      return;
+    }
+
+    const userVoiceState = guild.voiceStates.cache.get(member.id);
+
+    if (!userVoiceState || !userVoiceState.channelId) {
+      const voiceConnection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: guild.id,
+        adapterCreator: guild.voiceAdapterCreator,
+        selfMute: true,
+        selfDeaf: false,
+      });
+
+      voiceConnection.once('stateChange', (state) => {
+        if (state.status === 'CONNECTED') {
+          console.log('${member.displayName} is in ${guild.name}');
+          console.log('Bot joined voice channel ${channel.name} in ${guild.name}');
+        }
+      });
+
+      voiceConnection.on('error', (error) => {
+        console.error('Error in voice connection:', error);
+      });
+    }
+  } catch (error) {
+    console.error('Error checking and joining:', error);
+  }
+};
+
+channels.forEach((channelId) => {
+  const client = new Client({ checkUpdate: false });
+
+  client.once('ready', () => {
+    checkAndJoin(client, channelId);
+    setInterval(() => checkAndJoin(client, channelId), 5000);
+  });
+
+  client.login(process.env.token);
 });
-client.login(process.env.token);
